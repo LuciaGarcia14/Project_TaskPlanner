a14e6f01d6121e490e7249aa1136643f1c9dfa9c
LuciaGarcia14 <luciiagmon@gmail.com>
2025-04-17 21:37:16 +0200

    Quinto commit. Creamos la funcion de añadir evento.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 const form = document.getElementById('create-form');
 const table_event = document.getElementById('table-events');
 const table_body_event = document.getElementById('body_table-events');
+
+document.getElementById('btn_insert_event').addEventListener('click', async function(e){
+    e.preventDefault();
+
+
+});
+
+function add_event(event){
+const row = document.createElement('tr');
+row.innerHTML= `
+<td>${event.title}</td>
+<td>${event.detail}</td>
+<td>${event.date}</td>
+<td>${event.status}</td>
+`
+}; 
