e3bd4a7ddbbb5d0f895b6869e564eab7c176f12d
LuciaGarcia14 <luciiagmon@gmail.com>
2025-04-17 21:27:02 +0200

    Cuarto commit. Creamos el archivos js donde tendremos tres variables globales que utilizaremos en cualquier parte del script. Las variables guardan la tabla, el body de la tabla y el formulario.

---
+++ js/script.js
@@ -0,0 +1,3 @@
+const form = document.getElementById('create-form');
+const table_event = document.getElementById('table-events');
+const table_body_event = document.getElementById('body_table-events');
