ddddae8a8b6fd4a77286422814db8433e8d92f3b
LuciaGarcia14 <luciiagmon@gmail.com>
2025-04-18 14:23:26 +0200

    Sexto commit. Después de crear la función, rescatamos el botón y con el manejo del Dom recogemos todos los elementos del formulario y lo guardamos en localStorage y una vez guardadas llamamos a la función para añadir el nuevo evento a la tabla. Para que aparezcan toda la lista de eventos al cargar la pagina, cogemos los eventos guardados en el localStorage, los recorremos y lo mostramos en la tabla con la función. También hemos corregido un error, el cual se trataba de que la referencia del fichero script estaba mal escrito.

--- js/script.js
+++ js/script.js
@@ -2,9 +2,36 @@ const form = document.getElementById('create-form');
 const table_event = document.getElementById('table-events');
 const table_body_event = document.getElementById('body_table-events');
 
-document.getElementById('btn_insert_event').addEventListener('click', async function(e){
+const saved_events = localStorage.getItem('events');
+if(saved_events){
+    try{
+        const events = JSON.parse(saved_events);
+        events.forEach(event => add_event(event));
+        table_event.style.display = 'block';
+    }catch(error){
+        console.error('Error al mostrar la lista de eventos', error);
+    }
+}
+
+document.getElementById('btn_insert_event').addEventListener('click', function(e){
     e.preventDefault();
 
+    const title = document.getElementById('title-event').value;
+    const detail = document.getElementById('description-event').value;
+    const date = document.getElementById('date-event').value;
+    const status = document.getElementById('status-event').value;
+
+
+
+    const event_obj = {title, detail, date, status};
+
+    const events = JSON.parse(localStorage.getItem('events')) || [];
+    events.push(event_obj);
+    localStorage.setItem('events', JSON.stringify('events'));
+
+    add_event(event_obj);
+
+    form.reset();
 
 });
 
@@ -16,4 +43,6 @@ row.innerHTML= `
 <td>${event.date}</td>
 <td>${event.status}</td>
 `
+
+table_body_event.appendChild(row);
 }; 
